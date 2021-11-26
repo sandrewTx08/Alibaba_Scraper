64d928569fef6e04e9401b0b26b9b7845e6d61d9
Sander <89039740+sandrewTx08@users.noreply.github.com>
2021-11-26 17:46:20 -0300

    improving
    - async logic
    - segment the functions

--- alibaba_scraper.js
+++ alibaba_scraper.js
@@ -3,96 +3,94 @@ const cheerio = require('cheerio');
 const argv = process.argv.slice(2);
 
 
-const request = (index) => {
-    return new Promise( async (resolve, reject ) => {
-        try {
-            resolve( await axios.get(`https://www.alibaba.com/trade/search?SearchText=${argv[0]}&page=${index}`))
-        } catch (err) {
-            reject(err.message)
-        }
-    })
-}
+const catalog_ads_attributes = async (html) => {
+    let catalog_ads = []
+    let $ = cheerio.load(html)
+                
+    // Check reCAPTCHA
+    if (! $(html).find('.captcha-tips').length > 0 && ! html.includes('captcha')) { 
+        let catalog_box = $('.app-organic-search__list')
+        let catalog_box_item = catalog_box.find('.list-no-v2-outter')
+    
+        // Return catalog details if found
+        if (catalog_box_item.length > 0) {
+            
+            /* Return all details on page */
+            await catalog_box_item.each(async (i, el) => { 
+                catalog_ads.push({
 
-const alibaba_scraper = async (range) => {
-    let root = {
-        result: { 
-            name: argv[0],
-            catalog: []
-        }
+                    // Unique ID related ad post
+                    id: $(el).find('.J-img-switcher-item').attr('data-search-gt-img')
+                        || $(el).find('.elements-title-normal.one-line').attr('data-p4plog'),
+    
+                    // Title
+                    title: $(el).find('.elements-title-normal.one-line').attr('title') 
+                        || $(el).find('.elements-title-normal__outter').text(),
+                    
+                    // Price
+                    price: $(el).find('.elements-offer-price-normal.medium').attr('title') 
+                        || $(el).find('elements-offer-price-normal__price').text(),
+                    
+                    // Price discount
+                    price_discount: $(el).find('.element-promotion-shipping-price__price').text(),
+                    
+                    // Discount percentage
+                    price_discount_percentage: $(el).find('.elements-offer-slashprice-normal__discount').text(),
+                    
+                    // Avaliable pieces to buy
+                    pieces_stock: $(el).find('.element-offer-minorder-normal__value').text(),
+                    
+                    // Review title
+                    review_title: $(el).find('.seb-supplier-review__reviews.has-score').text(),
+                    
+                    // Review count
+                    review_count: $(el).find('.seb-supplier-review__review-count').text(),
+                                    
+                    // Link related to ad post
+                    link: $(el).find('.elements-title-normal.one-line').attr('href')
+                })
+            })
+            
+            return catalog_ads
+        }  
     }
     
-    let promises = []
-    for (let index=0; index < range; index++) {
-        promises.push(            
-            // Request page
-            request(index)
-            .then(response => {
-                return response.data
-            })
-            
-            // Parse HTML from page
-            .then(html => {
-                let $ = cheerio.load(html)
-                let catalog_box = $('.app-organic-search__list')
-                let catalog_box_item = catalog_box.find('.list-no-v2-outter')
-                
-                // Return catalog details if found
-                if (catalog_box_item.length > 0) {
-                    catalog_box_item.each((i, el) => { 
-                        let catalog = {
-                            /* Return all details on page */
-            
-                            // Unique ID related ad post
-                            id: $(el).find('.J-img-switcher-item').attr('data-search-gt-img')
-                                || $(el).find('.elements-title-normal.one-line').attr('data-p4plog'),
-            
-                            // Title
-                            title: $(el).find('.elements-title-normal.one-line').attr('title') 
-                                || $(el).find('.elements-title-normal__outter').text(),
-                            
-                            // Price
-                            price: $(el).find('.elements-offer-price-normal.medium').attr('title') 
-                                || $(el).find('elements-offer-price-normal__price').text(),
-                            
-                            // Price discount
-                            price_discount: $(el).find('.element-promotion-shipping-price__price').text(),
-                            
-                            // Discount percentage
-                            price_discount_percentage: $(el).find('.elements-offer-slashprice-normal__discount').text(),
-                            
-                            // Avaliable pieces to buy
-                            pieces_stock: $(el).find('.element-offer-minorder-normal__value').text(),
-                            
-                            // Review title
-                            review_title: $(el).find('.seb-supplier-review__reviews.has-score').text(),
-                            
-                            // Review count
-                            review_count: $(el).find('.seb-supplier-review__review-count').text(),
-                                            
-                            // Link related to ad post
-                            link: $(el).find('.elements-title-normal.one-line').attr('href')
-                        }
-                        root.result.catalog.push(catalog)
-                    }) 
-                } else { 
-                    // throw `Catalog total found: ${catalog_box_item.length}` 
-                }
-            })
-        )       
+    else {
+        throw Error('Alibaba is blocking network traffic, please resolve the reCAPTCHA')
     }
+}
 
-    // Return list of promises resolved
-    return Promise.all(promises)
-    .then(() => {
-        return root
+
+const request_catalog_ads_attributes = async (index) => {
+    // Request page
+    let html = await axios.get(`https://www.alibaba.com/trade/search?SearchText=${argv[0]}&page=${index}`)
+        .then(response => {
+            return response.data
+        })
+    
+    return await catalog_ads_attributes(html)
+    .then(attributes => { 
+        return attributes
     })
 }
 
-// Usage example
-alibaba_scraper(argv[1])
 
+const request_range_catalog_ads_attributes = async (range) => {
+    promises = []
+    
+    // Scrape the total of pages set by the range
+    for (let index=0; index < range; index++) {
+        promises.push(request_catalog_ads_attributes(index))
+    }
+    
+    return await Promise.all(promises)
+}
+
+
+// Usage example
+request_range_catalog_ads_attributes(argv[1])
 .then(result => {
     // Print JSON string
-    console.log(JSON.stringify(result)) 
+    console.log(JSON.stringify(result))
 })
 
