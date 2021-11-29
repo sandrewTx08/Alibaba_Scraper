99fa6d25cfe17b3ed599f4d499316761f6e0b124
Sander <89039740+sandrewTx08@users.noreply.github.com>
2021-11-29 14:50:21 -0300

    data cleaning
    - refactoring

--- alibaba_scraper/attributes.js
+++ alibaba_scraper/attributes.js
@@ -1,110 +1,188 @@
-const cheerio = require('cheerio');
 
 
 // All catalogs
 global.ads = []
 
 
-const catalog_ads = async (html) => {
-    let $ = cheerio.load(html)
-                
-    // Check reCAPTCHA
-    if (! $(html).find('.captcha-tips').length > 0 
-        && ! html.includes('captcha') ) { 
-        
-        let catalog_box = $('.app-organic-search__list')
-        let catalog_box_item = catalog_box.find('.list-no-v2-outter')
-    
-        // Check if found catalog ads
-        if ( catalog_box_item.length > 0 ) {
+const attribute = (el) => {
+    return {
+        // Unique ID related ad post
+        id: (() => {
+            a = el.find('.J-img-switcher-item').attr('data-search-gt-img') 
+            b = el.find('.elements-title-normal.one-line').attr('data-p4plog')
+            c = el.find('.J-favorite-manager-wrap-product.list-no-v2-favorite-container.J-sc-fav-item-wrap').attr('data-fav-id') 
+            d = el.find('.list-no-v2-inner.m-gallery-product-item-v2.img-switcher-parent').attr('data-ctrdot')
             
-            // Return all details on page
-            await catalog_box_item.each((i, el) => { 
-                ads.push( 
-                {
-                    // Unique ID related ad post
-                    id: (() => {
-                        a = $(el).find('.J-img-switcher-item').attr('data-search-gt-img') 
-                        b = $(el).find('.elements-title-normal.one-line').attr('data-p4plog')
-                        c = $(el).find('.J-favorite-manager-wrap-product.list-no-v2-favorite-container.J-sc-fav-item-wrap').attr('data-fav-id') 
-                        d = $(el).find('.list-no-v2-inner.m-gallery-product-item-v2.img-switcher-parent').attr('data-ctrdot')
-                        
-                        if ( a != undefined && a != '') {
-                            return a
-                        
-                        } else if ( b != undefined && b != '' ) {
-                            return b
-                        
-                        } else if ( c != undefined && c != '' ) {
-                            return c
-                        
-                        } else if ( d != undefined && d != '' ) {
-                            return d
-                        
-                        } else {
-                            return undefined
-                        }
-                    })(),
-                      
-                    // Title
-                    title: (() => {
-                        a = $(el).find('.elements-title-normal.one-line').attr('title') 
-                        b = $(el).find('.elements-title-normal__outter').text()
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else if ( b != undefined && b != '' ) {
+                return b
+            
+            } else if ( c != undefined && c != '' ) {
+                return c
+            
+            } else if ( d != undefined && d != '' ) {
+                return d
+            
+            } else {
+                return undefined
+            }
+        })(),
+          
+        // Title
+        title: (() => {
+            a = el.find('.elements-title-normal.one-line').attr('title') 
+            b = el.find('.elements-title-normal__outter').text()
 
-                        if ( a != undefined && a != '') {
-                            return a
-                        
-                        } else if ( b != undefined && b != '' ) {
-                            return b
-                        
-                        } else {
-                            return undefined
-                        }
-                    })(),
-                    
-                    // Price
-                    price: (() => {
-                        a = $(el).find('.elements-offer-price-normal').attr('title') 
-                        b = $(el).find('.elements-offer-price-normal__price').text() 
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else if ( b != undefined && b != '' ) {
+                return b
+            
+            } else {
+                return undefined
+            }
+        })(),
+        
+        // Price
+        price: (() => {
+            a = el.find('.elements-offer-price-normal').attr('title') 
+            b = el.find('.elements-offer-price-normal__price').text() 
+            c = el.find('.product-price.product-fob-wrap').text() 
 
-                        if ( a != undefined && a != '') {
-                            return a
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else if ( b != undefined && b != '' ) {
+                return b
+
+            } else if ( c != undefined && c != '' ) {
+                return c
+            
+            } else {
+                return undefined
+            } 
+        })(),
+        
+        // Price discount
+        price_discount: (() => {
+            a = el.find('.element-promotion-shipping-price__price').text()
+                .replace(/[^0-9]/g, '')
+
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else {
+                return undefined
+            } 
+        })(),
+        
+        // Discount percentage
+        price_discount_percentage: (() => {
+            a = el.find('.elements-offer-slashprice-normal__discount').text()
+            
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else {
+                return undefined
+            } 
+        })(),
+        
+        // Avaliable pieces to buy
+        pieces_stock: (() => {
+            a = el.find('.element-offer-minorder-normal__value').text()
+                .replace(/[^0-9]/g, '')
+            
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else if ( b != undefined && b != '' ) {
+                return b
+
+            } else if ( c != undefined && c != '' ) {
+                return c
+            
+            } else {
+                return undefined
+            } 
+        })(),
+
+        // Avaliable pieces to buy
+        pieces_stock_type: (() => {
+            a = el.find('.element-offer-minorder-normal__value').text()
+                .replace(/[0-9]/g, '')
+                .replace(/\s+/g, '')
+                .replace(/[&\/\\, +.]/g, '')
+            
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else {
+                return undefined
+            } 
+        })(),
+        
+        // Review title
+        review_title: (() => {
+            a = el.find('.seb-supplier-review__reviews.has-score').text()
+                .replace(/\"/g, '')
+
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else {
+                return undefined
+            } 
+        })(),
+        
+        // Review count
+        review_count: (() => {
+            a = el.find('.seb-supplier-review__review-count').text()
+                .replace(/[^0-9]/g, '')
+            
+            if ( a != undefined && a != '') {
+                return a
+    
+            } else {
+                return undefined
+            } 
+        })(),
                         
-                        } else if ( b != undefined && b != '' ) {
-                            return b
-                        
-                        } else {
-                            return undefined
-                        } 
-                    })(),
-                    
-                    // Price discount
-                    price_discount: $(el).find('.element-promotion-shipping-price__price').text(),
-                    
-                    // Discount percentage
-                    price_discount_percentage: $(el).find('.elements-offer-slashprice-normal__discount').text(),
-                    
-                    // Avaliable pieces to buy
-                    pieces_stock: $(el).find('.element-offer-minorder-normal__value').text(),
-                    
-                    // Review title
-                    review_title: $(el).find('.seb-supplier-review__reviews.has-score').text(),
-                    
-                    // Review count
-                    review_count: $(el).find('.seb-supplier-review__review-count').text(),
-                                    
-                    // Link related to ad post
-                    link: $(el).find('.elements-title-normal.one-line').attr('href')
-                })
-            })
-        }  
+        // Link related to ad post
+        link: (() => {
+            a = el.find('.elements-title-normal.one-line').attr('href')
+                .replace(/[//]/, 'https:/')
+            
+            if ( a != undefined && a != '') {
+                return a
+            
+            } else {
+                return undefined
+            } 
+        })()
     }
     
-    else {
-        throw Error('Alibaba is blocking network traffic, please resolve the reCAPTCHA')
-    }
-
 }
+
+
+const catalog_ads = ($) => {
+    let catalog_box = $('.app-organic-search__list')
+    let catalog_box_item = catalog_box.find('.list-no-v2-outter')
+
+    // Check if found catalog ads
+    if ( catalog_box_item.length > 0 ) {
+        
+        // Return all details on page
+        catalog_box_item.each((i, el) => {          
+            ads.push(attribute($(el)))
+        })
+
+    }  
+}
+
 
 module.exports = { catalog_ads }
 
--- alibaba_scraper/request.js
+++ alibaba_scraper/request.js
@@ -1,27 +1,47 @@
 const axios = require('axios');
 const attributes = require('./attributes.js');
-  
+const cheerio = require('cheerio');
+
 
 const catalog_ads_attributes = async (index) => {
-    // Search keyword URL
-    let url = `https://www.alibaba.com/trade/search?SearchText=${config.search}`
-
+    let url = `https://www.alibaba.com/trade/search`
+    url += '?'
+        
     // URL parameters: 
-        url += '&viewtype=L' // Try to fix price attribute not found
-        url += `&page=${index}` // Set URL index
-    
+    url += `SearchText=${config.search}` // Search keyword URL
+    url += `&page=${index}` // Set URL index
+    url += '&CatId='
+    url += '&fsb=y&'
+    url += '&selectedTab=product_en'
+    url += '&IndexArea=product_en'
+    url += '&viewtype=L' // Default view type
+
     // Request page
-    await axios.get(url, config.options)  
+    await axios.get(url, config.options)      
         .then(response => {
             return response.data
         })
-        
-        // Scrape
+            
         .then(html => {
-            attributes.catalog_ads(html)
+            if (! html.includes('captcha')) {
+                return cheerio.load(html) 
+            
+            } else {
+                throw Error('Alibaba is blocking network traffic, please resolve the reCAPTCHA')
+            }
+
         })
-    
-    
+
+        // Scrape
+        .then($ => {
+            if (! $(this).find('.captcha-tips').length > 0 ) { 
+                attributes.catalog_ads($)
+            
+            } else {
+                throw Error('Alibaba is blocking network traffic, please resolve the reCAPTCHA')
+            }
+        })
+
 }
 
 
@@ -36,6 +56,7 @@ const range_catalog_ads_attributes = (range) => {
     return Promise.all(promises)
 
 }
+
 
 module.exports = { 
     catalog_ads_attributes, 
